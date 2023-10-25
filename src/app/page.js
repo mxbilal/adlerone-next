4f72bb8615eabf590f04635b94dcdb6cbec48de4
bilal <mxbilal0@gmail.com>
2023-10-25 10:22:06 +0500

    server error fixed

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,11 @@ export default function Home() {
 	}, [])
 
 	if (firstRender) {
-		return <></>
+		return <div style={{
+			width:'100%',
+			height:'100vh',
+			background:'white'
+		}}></div>
 	}
 	return (
 		<div>	
